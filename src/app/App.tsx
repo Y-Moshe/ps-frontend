1bb1e0b41c05259c0f9d421549aa3c21d6fedcae
y-moshe <moshe.nehemiah@gmail.com>
2021-09-23 23:26:55 +0300

    structure app, empty content

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import { Footer, Header } from './components';
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <p>App works</p>
+      <Footer />
+    </div>
+  );
+}
