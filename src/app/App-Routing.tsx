49d9ab29245805fc9d1a7c543ba22f427eeb1a8e
y-moshe <moshe.nehemiah@gmail.com>
2021-09-24 17:04:46 +0300

    app routing config

---
+++ src/app/App-Routing.tsx
@@ -0,0 +1,25 @@
+import { Switch, Route } from 'react-router-dom';
+
+import {
+  HomePage,
+  AboutPage,
+  ContactPage,
+  ProductsPage,
+  ProductReviewPage,
+  CartPage,
+  PageNotFound
+} from './pages';
+
+export function AppRouting() {
+  return (
+    <Switch>
+      <Route path = "/"             component = { HomePage } exact />
+      <Route path = "/about"        component = { AboutPage } />
+      <Route path = "/contact"      component = { ContactPage } />
+      <Route path = "/products"     component = { ProductsPage } />
+      <Route path = "/products/:id" component = { ProductReviewPage } />
+      <Route path = "/cart"         component = { CartPage } />
+      <Route path = "*"             component = { PageNotFound } />
+    </Switch>
+  )
+}
