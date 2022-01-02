f4e57ff45bc92b8412a6a69e3332a1ebc54c98be
Y-Moshe <moshe.nehemiah@gmail.com>
2022-01-02 19:20:27 +0200

    App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,12 @@ import { useEffect } from 'react';
 
 import { Footer, Header } from './components';
 import { AppRouting } from './App-Routing';
+
 import { version } from '../../package.json';
 
 export default function App() {
   useEffect(() => {
-    console.log(`v${ version }`);
+    console.log(`Running: v${ version }`);
   }, []);
 
   return (
