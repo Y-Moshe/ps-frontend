278d8b85fafdca983b7fc3c1561ecb6f37768638
Y-Moshe <moshe.nehemiah@gmail.com>
2022-01-02 19:22:48 +0200

    add index exports

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,2 +1,4 @@
 export { Header } from './Layout/Header';
 export { Footer } from './Layout/Footer';
+export { LoginDialog } from './UI/Dialogs/LoginDialog';
+export { RecoveryDialog } from './UI/Dialogs/RecoveryDialog';
