c037dd9605fc8afcfc9f4e96e4f481fed6e46551
Y-Moshe <moshe.nehemiah@gmail.com>
2022-01-02 19:21:03 +0200

    add Redirect to /home

--- src/app/App-Routing.tsx
+++ src/app/App-Routing.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 
 import {
   HomePage,
@@ -13,12 +13,16 @@ import {
 export function AppRouting() {
   return (
     <Switch>
+      {/* Routes paths */}
       <Route path = "/"             component = { HomePage } exact />
       <Route path = "/about"        component = { AboutPage } />
       <Route path = "/contact"      component = { ContactPage } />
       <Route path = "/products/:id" component = { ProductReviewPage } />
       <Route path = "/products"     component = { ProductsPage } />
       <Route path = "/cart"         component = { CartPage } />
+      {/* Redirects Cases */}
+      <Redirect path = "/home" to = "/" />
+      {/* 404 Case */}
       <Route path = "*"             component = { PageNotFound } />
     </Switch>
   )
