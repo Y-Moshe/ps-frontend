7b067e66062fad755939843798a297913ff92daf
Y-Moshe <moshe.nehemiah@gmail.com>
2022-01-02 19:22:25 +0200

    RecoveryDialog component

---
+++ src/app/components/UI/Dialogs/RecoveryDialog.tsx
@@ -0,0 +1,56 @@
+import {
+  Button,
+  TextField,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  InputAdornment
+} from '@mui/material';
+import { Email } from '@mui/icons-material';
+
+interface RecoveryDialogProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onRecoveryRequest: ( email: string ) => void;
+}
+
+export function RecoveryDialog( props: RecoveryDialogProps ) {
+  return (
+    <Dialog
+      open = { props.isOpen }
+      onBackdropClick = { props.onClose }>
+      <DialogTitle>Password Recovery</DialogTitle>
+
+      <DialogContent>
+        <TextField
+          InputProps = {{
+            startAdornment: (
+              <InputAdornment position = "start">
+                <Email />
+              </InputAdornment>
+            )
+          }}
+          variant = "filled"
+          label   = "Username"
+          fullWidth
+        />
+        <DialogContentText>We will send to your email a link to reset the password! check your mailbox for next steps.</DialogContentText>
+      </DialogContent>
+
+      <DialogActions>
+        <Button
+          color   = "error"
+          onClick = { props.onClose }>
+            Close
+        </Button>
+        <Button
+          color   = "success"
+          onClick = { () => props.onRecoveryRequest( 'test@test.com' ) }>
+            Recover
+        </Button>
+      </DialogActions>
+    </Dialog>
+  )
+}
